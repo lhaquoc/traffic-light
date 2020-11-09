cefd3fd4c28e070817ec96db72ca124327d5bc38
lhaquoc <38935979+lhaquoc@users.noreply.github.com>
2020-11-09 17:00:00 +0700

    first commit
    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import TrafficLight from "./components/TrafficLight";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const RED = 0;
+const YELLOW = 1;
+const GREEN = 2;
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentColor: RED,
+    };
+    setInterval(() => {
+      this.setState({
+        currentColor: this.getNextColor(this.state.currentColor),
+      });
+    }, 1000);
+  }
+
+  getNextColor(color) {
+    switch (color) {
+      case RED:
+        return YELLOW;
+      case YELLOW:
+        return GREEN;
+      default:
+        return RED;
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <TrafficLight currentColor={this.state.currentColor}/>
+        {}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TrafficLight.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import './TrafficLight.css'
+
+const RED = 0;
+const YELLOW = 1;
+const GREEN = 2;
+
+class TrafficLight extends Component {
+    render() {
+        const { currentColor } = this.props;
+        console.log('Rendering...', currentColor);
+        return(
+            <div className="TrafficLight">
+                <div className={classNames('bulb', 'red', {
+                    active: currentColor === RED
+                })}/>
+                <div className={classNames('bulb', 'yellow', {
+                    active: currentColor === YELLOW
+                })}/>
+                <div className={classNames('bulb', 'green', {
+                    active: currentColor === GREEN
+                })}/>
+            </div>
+        );
+    }
+}
+
+export default TrafficLight;
